2030456df7481014d994cdd2b81d1494b543b978
Jonathan Martin <jmartin@iw2.fr>
2015-07-26 11:37:42 +0200

    map container volumes

--- container.go
+++ container.go
@@ -1,14 +1,21 @@
 package chronos
 
 type Container struct {
-	Type    string `json:"type"`
-	Image   string `json:"image"`
-	Network string `json:"network"`
+	Type    string   `json:"type"`
+	Image   string   `json:"image"`
+	Network string   `json:"network"`
+	Volumes []Volume `json:"volumes"`
 }
 
 type EnvVar struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
+}
+
+type Volume struct {
+	HostPath      string `json:"hostPath"`
+	ContainerPath string `json:"containerPath"`
+	Mode          string `json:"mode"`
 }
 
 // NewContainer creates a new Container assignment
